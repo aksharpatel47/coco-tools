787edb1b30182df53fc599ce40169f23f005f6c0
Akshar Patel <akshar.patel.47@gmail.com>
2019-07-07 23:21:01 -0700

    feat: add tfrecord functions
    Also, remove obsolete tfrecord functions

--- tf_record.py
+++ tf_record.py
@@ -1,141 +1,133 @@
-from .image import Image
-import tensorflow as tf
-from object_detection.utils import dataset_util
-from concurrent.futures import ProcessPoolExecutor
 import copyreg
-from functools import partial
+import glob
+import hashlib
+import io
 import os
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+from typing import List
+
+import tensorflow as tf
+from object_detection.utils import dataset_util, label_map_util
+from PIL import Image
+
+from .dataset import ImageDataSet
+# from .image import Image
+from .labels import (convert_label_dict_to_obj, convert_labels_to_names,
+                     get_data_obj_from_xml, get_label_category_dict,
+                     hflip_label)
 
 
-def write_inference_record(file_name, images):
-    output_path = "{}.record".format(file_name)
-    writer = tf.python_io.TFRecordWriter(output_path)
-    for image in images:
-        writer.write(image.tfrecord.SerializeToString())
-    writer.close()
+def create_tf_record(data, key, encoded_jpg, label_map_dict, flipped=False, ignore_difficult_instances=True):
+    width = int(data['size']['width'])
+    height = int(data['size']['height'])
 
-
-def write_record(set_name, images, height, width):
-    output_path = "{}.record".format(set_name)
-    writer = tf.python_io.TFRecordWriter(output_path)
-    crosswalk_tf = partial(create_crosswalk_tf, height=height, width=width)
-    with ProcessPoolExecutor() as executor:
-        for example in executor.map(crosswalk_tf, images):
-            if example and len(example) == 1:
-                writer.write(example[0].SerializeToString())
-            elif example and len(example) == 2:
-                writer.write(example[0].SerializeToString())
-                writer.write(example[1].SerializeToString())
-    writer.close()
-
-
-def read_output_record(file_name):
-    data = {}
-
-    for example in tf.python_io.tf_record_iterator(file_name):
-        json_message = tf.train.Example.FromString(example)
-        features = json_message.features.feature
-        scores = features['image/detection/score'].float_list.value
-        labels = features['image/detection/label'].int64_list.value
-        file_name = features['image/filename'].bytes_list.value[0].decode(
-            "utf-8")
-        image_name, _ = os.path.splitext(file_name)
-        detected_labels = set()
-        for i, label in enumerate(labels):
-            if scores[i] > 0.5:
-                detected_labels.add(int(label))
-        data[image_name] = list(detected_labels)
-
-    return data
-
-
-def get_flipped_crosswalk_tf(image, width, height):
-    filename = os.path.splitext(image.name)[0] + "_flipped.jpg"
-    image_format = b'jpg'
-
-    encoded_jpg = image.get_flipped_image()
-
-    flipped_labels = image.get_flipped_labels()
-
-    xmins = []
-    xmaxs = []
-    ymins = []
-    ymaxs = []
-    classes_text = []
+    xmin = []
+    ymin = []
+    xmax = []
+    ymax = []
     classes = []
+    classes_text = []
+    truncated = []
+    poses = []
+    difficult_obj = []
 
-    for lbl_box in flipped_labels:
-        xmins.append(float(lbl_box.bb.left) / width)
-        ymins.append(float(lbl_box.bb.top) / height)
-        xmaxs.append(float(lbl_box.bb.right) / width)
-        ymaxs.append(float(lbl_box.bb.bottom) / height)
-        classes_text.append(lbl_box.lbl.name.encode("utf8"))
-        classes.append(lbl_box.lbl.id)
+    if flipped and data.get("object"):
+        for obj in data["object"]:
+            hflip_label(width, obj)
 
-    filename = image.name.encode("utf8")
+    if 'object' in data:
+        for obj in data['object']:
+            difficult = bool(int(obj['difficult']))
+            if ignore_difficult_instances and difficult:
+                continue
 
-    tf_example = tf.train.Example(features=tf.train.Features(feature={
+            difficult_obj.append(int(difficult))
+
+            xmin.append(float(obj['bndbox']['xmin']) / width)
+            ymin.append(float(obj['bndbox']['ymin']) / height)
+            xmax.append(float(obj['bndbox']['xmax']) / width)
+            ymax.append(float(obj['bndbox']['ymax']) / height)
+            classes_text.append(obj['name'].encode('utf8'))
+            classes.append(label_map_dict[obj['name']])
+            truncated.append(int(obj['truncated']))
+            poses.append(obj['pose'].encode('utf8'))
+
+    example = tf.train.Example(features=tf.train.Features(feature={
         'image/height': dataset_util.int64_feature(height),
         'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(filename),
-        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/filename': dataset_util.bytes_feature(data['filename'].encode('utf8')),
+        'image/source_id': dataset_util.bytes_feature(data['filename'].encode('utf8')),
+        'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
         'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-        'image/format': dataset_util.bytes_feature(image_format),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
         'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
         'image/object/class/label': dataset_util.int64_list_feature(classes),
+        'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
+        'image/object/truncated': dataset_util.int64_list_feature(truncated),
+        'image/object/view': dataset_util.bytes_list_feature(poses),
     }))
 
-    return tf_example
+    return example
 
 
-def create_crosswalk_tf(image, height=640, width=480):
-    filename = image.name
-    image_format = b'jpg'
+def dict_to_tf_example(data,
+                       dataset_directory,
+                       label_map_dict,
+                       image_types):
+    image_subdirectory = ""
+    img_path = os.path.join(
+        data['folder'], image_subdirectory, data['filename'])
+    full_path = os.path.join(dataset_directory, img_path)
 
-    try:
-        with tf.gfile.GFile(image.path, "rb") as fid:
-            encoded_jpg = fid.read()
-    except:
-        return None
+    examples = []
 
-    xmins = []
-    xmaxs = []
-    ymins = []
-    ymaxs = []
-    classes_text = []
-    classes = []
+    vals = []
 
-    for lbl_box in image.labels:
-        xmins.append(float(lbl_box.bb.left) / width)
-        ymins.append(float(lbl_box.bb.top) / height)
-        xmaxs.append(float(lbl_box.bb.right) / width)
-        ymaxs.append(float(lbl_box.bb.bottom) / height)
-        classes_text.append(lbl_box.lbl.name.encode("utf8"))
-        classes.append(lbl_box.lbl.id)
+    if "default" in image_types:
+        im = Image.open(full_path)
+        img = io.BytesIO()
+        im.save(img, format="JPEG")
+        encoded_jpg = img.getvalue()
+        key = hashlib.sha256(encoded_jpg).hexdigest()
+        examples.append(create_tf_record(
+            data, key, encoded_jpg, label_map_dict))
 
-    filename = image.name.encode("utf8")
+    if "flipped" in image_types:
+        im = Image.open(full_path)
+        fim = im.transpose(Image.FLIP_LEFT_RIGHT)
+        img = io.BytesIO()
+        fim.save(img, format="JPEG")
+        encoded_jpg = img.getvalue()
+        data["filename"] = data["filename"].replace(".jpg", "_flipped.jpg")
+        key = hashlib.sha256(encoded_jpg).hexdigest()
 
-    tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(filename),
-        'image/source_id': dataset_util.bytes_feature(filename),
-        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-        'image/format': dataset_util.bytes_feature(image_format),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
-    }))
+        examples.append(create_tf_record(
+            data, key, encoded_jpg, label_map_dict, flipped=True))
 
-    if len(image.labels) >= 1:
-        return (tf_example, get_flipped_crosswalk_tf(image, width, height))
-        # return (tf_example,)
-    else:
-        return (tf_example,)
+    return examples
+
+
+def write_tf_record(inputs: List[ImageDataSet], label_path: str, output_file: str):
+    writer = tf.python_io.TFRecordWriter(output_file)
+
+    label_dict = label_map_util.get_label_map_dict(label_path)
+    label_categories_dict = get_label_category_dict(label_dict)
+
+    for inp in inputs:
+        all_input_xmls = glob.glob(os.path.join(inp.folder_name, "**", "*.xml"), recursive=True)
+        all_input_xmls = list(filter(lambda x: os.path.isfile(x.replace(".xml", ".jpg")), all_input_xmls))
+
+        for xml_file in all_input_xmls:
+            data = get_data_obj_from_xml(xml_file)
+            convert_labels_to_names(data, label_dict, label_categories_dict)
+            tf_examples = dict_to_tf_example(data, inp.folder_name, label_dict, inp.image_augmentation)
+
+            for tfe in tf_examples:
+                writer.write(tfe.SerializeToString())
+
+    writer.close()
