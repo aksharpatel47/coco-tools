b4fb5a5619d3cbe81415b840e62fc919d60cfdae
Akshar Patel <akshar.patel.47@gmail.com>
2019-07-07 23:20:02 -0700

    feat: add more label functions & tests

--- labels.py
+++ labels.py
@@ -1,42 +1,85 @@
 from collections import namedtuple
 import os
 from typing import Optional
+from lxml import etree
+from object_detection.utils import dataset_util
 
 BoundingBox = namedtuple('BoundingBox', ['left', 'top', 'right', 'bottom'])
 
+
+def get_label_category_dict(label_dict):
+    scategory = {}
+    for k in label_dict.keys():
+        scategory["".join(map(lambda x: x[0], k.split("-")))] = k
+
+    return scategory
+
+
 def hflip_label(image_width, label):
-  xmax = int(image_width) - int(label["bndbox"]["xmin"])
-  xmin = int(image_width) - int(label["bndbox"]["xmax"])
-  
-  if xmax < 0 or xmin < 0:
-    raise Exception("Zero", xmax, xmin)
-  
-  if xmax > image_width or xmin > image_width:
-    raise Exception("Out of Bounds", xmax, xmin)
-    
-  if xmin >= xmax:
-    raise Exception("Comparison", xmin, xmax)
-  
-  label["bndbox"]["xmin"] = str(xmin)
-  label["bndbox"]["xmax"] = str(xmax)
+    xmax = int(image_width) - int(label["bndbox"]["xmin"])
+    xmin = int(image_width) - int(label["bndbox"]["xmax"])
+
+    if xmax < 0 or xmin < 0:
+        raise Exception("Zero", xmax, xmin)
+
+    if xmax > image_width or xmin > image_width:
+        raise Exception("Out of Bounds", xmax, xmin)
+
+    if xmin >= xmax:
+        raise Exception("Comparison", xmin, xmax)
+
+    label["bndbox"]["xmin"] = str(xmin)
+    label["bndbox"]["xmax"] = str(xmax)
 
 
 def convert_label_dict_to_obj(data, label_map_dict):
-  boxes = []
-  scores = []
-  classes = []
-  
-  if 'object' in data:  
-    for obj in data['object']:
-      if bool(int(obj['difficult'])):
-        continue
+    boxes = []
+    scores = []
+    classes = []
 
-      scores.append(1.0)
-      classes.append(label_map_dict[obj['name']])
-      boxes.append([float(obj['bndbox']['ymin']),float(obj['bndbox']['xmin']),float(obj['bndbox']['xmax']),float(obj['bndbox']['ymax'])])
+    if 'object' in data:
+        for obj in data['object']:
+            if bool(int(obj['difficult'])):
+                continue
 
-  return {"image_id": data["filename"], "groundtruth_boxes": boxes, "groundtruth_scores": scores, "groundtruth_classes": classes}
-  
+            scores.append(1.0)
+            classes.append(label_map_dict[obj['name']])
+            boxes.append([float(obj['bndbox']['ymin']), float(obj['bndbox']['xmin']), float(
+                obj['bndbox']['xmax']), float(obj['bndbox']['ymax'])])
+
+    return {"image_id": data["filename"], "groundtruth_boxes": boxes, "groundtruth_scores": scores, "groundtruth_classes": classes}
+
+
+def get_data_obj_from_xml(path_str):
+    with open(path_str) as fd:
+        xml_str = fd.read()
+        xml_str = xml_str.replace("<object></object>", "")
+
+    xml = etree.fromstring(xml_str)
+    data = dataset_util.recursive_parse_xml_to_dict(xml)["annotation"]
+    data["folder"] = os.path.basename(os.path.split(path_str)[0])
+
+    return data
+
+
+def convert_labels_to_names(data, label_dict, label_categories_dict):
+    labels_to_skip = []
+    if data.get("object"):
+        if len(data["object"]) > 0:
+            for i, obj in enumerate(data["object"]):
+                if len(obj["name"]) <= 2:
+                    if label_categories_dict.get(obj["name"]):
+                        data["object"][i]["name"] = label_categories_dict[obj["name"]]
+                    else:
+                        labels_to_skip.append(obj)
+                elif not label_dict.get(obj["name"]):
+                    labels_to_skip.append(obj)
+
+            for lbl in labels_to_skip:
+                data["object"].remove(lbl)
+        else:
+            del data["object"]
+
 
 def get_bb_size(bb):
     width = bb.right - bb.left
--- labels_test.py
+++ labels_test.py
@@ -1,5 +1,21 @@
-from .labels import hflip_label, convert_label_dict_to_obj
+import os
+from .labels import hflip_label, convert_label_dict_to_obj, convert_labels_to_names, get_data_obj_from_xml, get_label_category_dict
 from object_detection.utils import label_map_util
+
+label_path = os.path.join("sample_files", "labels.pbtxt")
+label_dict = label_map_util.get_label_map_dict(label_path)
+label_categories_dict = get_label_category_dict(label_dict)
+
+
+def test_get_label_category_dict():
+    label_category_dict = get_label_category_dict(label_dict)
+
+    groundtruth_category_dict = {
+        "w": "walksignal",
+        "wa": "walksignal-alone"
+    }
+
+    assert label_category_dict == groundtruth_category_dict
 
 
 def test_hflip_label():
@@ -30,21 +46,180 @@ def test_hflip_label():
     assert lbl["bndbox"]["xmax"] == '635'
 
 
-def test_convert_label_dict_to_obj():
-    label_dict = label_map_util.get_label_map_dict("sample_labels.pbtxt")
-    data = {
-        "filename": "anyfile.jpg",
+def test_get_data_obj_from_xml():
+    no_obj_path_str = os.path.join("sample_files", "no_objects.xml")
+    data = get_data_obj_from_xml(no_obj_path_str)
+
+    groundtruth_data = {
+        "folder": "sample_files",
+        "filename": "33.6491385170296_-112.20791456542071_0_0.jpg",
+        "source": {
+            "database": "Unknown"
+        },
+        "size": {
+            "width": "512",
+            "height": "512",
+            "depth": "3"
+        },
+        "segmented": "0"
+    }
+
+    assert data == groundtruth_data
+
+    with_labels_path_str = os.path.join("sample_files", "with_labels.xml")
+    data = get_data_obj_from_xml(with_labels_path_str)
+
+    groundtruth_data = {
+        "folder": "sample_files",
+        "filename": "33.6693057958454_-112.12601933960218_270_0.jpg",
+        "source": {
+            "database": "Unknown"
+        },
+        "size": {
+            "width": "512",
+            "height": "512",
+            "depth": "3"
+        },
+        "segmented": "0",
         "object": [
-            {"name": "walksignal", "difficult": "0", "bndbox": {
-                "xmin": "1", "xmax": "2", "ymin": "0", "ymax": "3"}, }
+            {
+                "name": "w",
+                "pose": "Unspecified",
+                "truncated": "0",
+                "difficult": "0",
+                "bndbox": {
+                    "xmin": "85",
+                    "xmax": "102",
+                    "ymin": "207",
+                    "ymax": "270"
+                }
+            },
+            {
+                "name": "w",
+                "pose": "Unspecified",
+                "truncated": "0",
+                "difficult": "0",
+                "bndbox": {
+                    "xmin": "397",
+                    "xmax": "426",
+                    "ymin": "227",
+                    "ymax": "272"
+                }
+            }
         ]
     }
 
-    result_data = {
-        "image_id": "anyfile.jpg",
-        "groundtruth_boxes": [[0.0, 1.0, 2.0, 3.0]],
-        "groundtruth_scores": [1.0],
-        "groundtruth_classes": [1]
+    assert data == groundtruth_data
+
+
+def test_convert_labels_to_names():
+    with_labels_path_str = os.path.join("sample_files", "with_labels.xml")
+    data = get_data_obj_from_xml(with_labels_path_str)
+    convert_labels_to_names(data, label_dict, label_categories_dict)
+
+    groundtruth_data = {
+        "folder": "sample_files",
+        "filename": "33.6693057958454_-112.12601933960218_270_0.jpg",
+        "source": {
+            "database": "Unknown"
+        },
+        "size": {
+            "width": "512",
+            "height": "512",
+            "depth": "3"
+        },
+        "segmented": "0",
+        "object": [
+            {
+                "name": "walksignal",
+                "pose": "Unspecified",
+                "truncated": "0",
+                "difficult": "0",
+                "bndbox": {
+                    "xmin": "85",
+                    "xmax": "102",
+                    "ymin": "207",
+                    "ymax": "270"
+                }
+            },
+            {
+                "name": "walksignal",
+                "pose": "Unspecified",
+                "truncated": "0",
+                "difficult": "0",
+                "bndbox": {
+                    "xmin": "397",
+                    "xmax": "426",
+                    "ymin": "227",
+                    "ymax": "272"
+                }
+            }
+        ]
     }
 
-    assert result_data == convert_label_dict_to_obj(data, label_dict)
+    assert data == groundtruth_data
+
+    with_labels_path_str = os.path.join(
+        "sample_files", "with_extra_labels.xml")
+    data = get_data_obj_from_xml(with_labels_path_str)
+    convert_labels_to_names(data, label_dict, label_categories_dict)
+
+    groundtruth_data = {
+        "folder": "sample_files",
+        "filename": "33.37889865057694_-111.60161530847279_90_0.jpg",
+        "source": {
+            "database": "Unknown"
+        },
+        "size": {
+            "width": "512",
+            "height": "512",
+            "depth": "3"
+        },
+        "segmented": "0",
+        "object": [
+            {
+                "name": "walksignal",
+                "pose": "Unspecified",
+                "truncated": "0",
+                "difficult": "0",
+                "bndbox": {
+                    "xmin": "381",
+                    "xmax": "396",
+                    "ymin": "231",
+                    "ymax": "278"
+                }
+            }
+        ]
+    }
+
+    assert data == groundtruth_data
+
+
+def test_convert_label_dict_to_obj():
+    with_labels_path_str = os.path.join("sample_files", "with_labels.xml")
+    data = get_data_obj_from_xml(with_labels_path_str)
+    convert_labels_to_names(data, label_dict, label_categories_dict)
+    data_obj = convert_label_dict_to_obj(data, label_dict)
+
+    groundtruth_data = {
+        "image_id": "33.6693057958454_-112.12601933960218_270_0.jpg",
+        "groundtruth_boxes": [[207.0, 85.0, 102.0, 270.0], [227.0, 397.0, 426.0, 272.0]],
+        "groundtruth_scores": [1.0, 1.0],
+        "groundtruth_classes": [1, 1]
+    }
+
+    assert data_obj == groundtruth_data
+
+    no_obj_path_str = os.path.join("sample_files", "no_objects.xml")
+    data = get_data_obj_from_xml(no_obj_path_str)
+    convert_labels_to_names(data, label_dict, label_categories_dict)
+    data_obj = convert_label_dict_to_obj(data, label_dict)
+
+    groundtruth_data = {
+        "image_id": "33.6491385170296_-112.20791456542071_0_0.jpg",
+        "groundtruth_boxes": [],
+        "groundtruth_scores": [],
+        "groundtruth_classes": []
+    }
+
+    assert data_obj == groundtruth_data
