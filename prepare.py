7523fd64b23beb77cd5cd803fce890275a8cc627
Akshar Patel <akspatel@paypal.com>
2019-07-10 08:49:43 -0700

    feat: add prepare_records function

---
+++ prepare.py
@@ -0,0 +1,40 @@
+from dataset import ImageDataSet, ImageCollection
+from typing import List
+from tf_record import write_tf_record
+import requests
+
+
+def prepare_records(collections: List[ImageCollection], labels_url: str):
+    label_path = "labels.pbtxt"
+    f = requests.get(labels_url)
+    open(label_path, "wb").write(f.content)
+
+    for collection in collections:
+        write_tf_record(collection.datasets, label_path,
+                        f"{collection.name}.record")
+
+
+if __name__ == "__main__":
+    collections = [
+        ImageCollection("training", [
+            ImageDataSet(
+                "WalkSignLabel",
+                "https://www.dropbox.com/s/hwpdrib721g34l4/WalkSignLabel.zip?dl=1",
+                ["default", "flipped"]
+            ),
+            ImageDataSet(
+                "merged_intersection_inference",
+                "https://www.dropbox.com/s/8tkjwy6mzdkw4qw/merged_intersection_inference.zip?dl=1",
+                ["flipped"]
+            )
+        ]),
+        ImageCollection("validation", [
+            ImageDataSet(
+                "merged_intersection_inference",
+                "https://www.dropbox.com/s/8tkjwy6mzdkw4qw/merged_intersection_inference.zip?dl=1",
+                ["default"]
+            )
+        ])
+    ]
+
+    prepare_records(collections, "sample_files/labels.pbtxt")
