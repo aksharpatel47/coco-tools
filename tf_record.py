86700cd284c3332f1d7f3a243030ceedc250ba64
Akshar Patel <akspatel@paypal.com>
2019-07-20 02:19:53 -0700

    fix: add  code to skip difficult labels

--- tf_record.py
+++ tf_record.py
@@ -142,6 +142,11 @@ def write_tf_record(inputs: List[ImageDataSet], label_path: str, output_file: st
                 if inp.is_ground_truth:
                     label_json.add_data(data)
 
+                difficult_labels = filter(lambda x: x == '1', map(lambda x: x.difficult, data["object"]))
+
+                if len(difficult_labels) > 0:
+                    continue
+
                 try:
                     tf_examples = dict_to_tf_example(
                         data, inp.folder_name, label_dict, inp.image_augmentation)
